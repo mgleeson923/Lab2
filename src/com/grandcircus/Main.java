ed906588a1bc5bc400ff91e69a9eddcc57ceae65
mgleeson923 <mgleeson923@gmail.com>
2017-06-27 20:21:33 -0400

    Initial Commit

---
+++ src/com/grandcircus/Main.java
@@ -0,0 +1,57 @@
+package com.grandcircus;
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String userName = null;
+        int userNum = 0;
+        String repeat = null;
+
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        userName = scnr.next();
+        do {
+
+            System.out.print("Enter an integer ranging from 1 to 100: ");
+            userNum = scnr.nextInt();
+
+            if ((userNum % 2) != 0) {
+                System.out.println(userName + ", " + userNum + " is Odd.");
+            }
+            if ((userNum >= 2) && (userNum <= 25) && ((userNum % 2) == 0)) {
+                System.out.println(userName + ", " + userNum + " is Even and less than 25");
+            }
+            if ((userNum >= 26) && (userNum <= 60)) {
+                System.out.println(userName + ", " + userNum + " is Even.");
+            }
+            if (((userNum % 2) != 0) && (userNum > 60)) {
+                System.out.println(userName + ", " + userNum + " is Odd and over 60.");
+            }
+
+            System.out.println("Would you like to play again? Y/N?");
+            repeat = scnr.next();
+
+        } while (!repeat.equals("N") && !repeat.equals("n"));
+
+        System.out.println("Thanks for playing " + userName + "!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
